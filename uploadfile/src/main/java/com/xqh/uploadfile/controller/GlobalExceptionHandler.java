6e4e2cb0d18a3f2796c272d286a8dd0a0ebc7a79
xiaoran6822 <xiaoran6822.github.io>
2019-04-08 00:34:01 +0800

    uploadbase64

---
+++ uploadfile/src/main/java/com/xqh/uploadfile/controller/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.xqh.uploadfile.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ * @Author：zhsz
+ * @Description:
+ * @Date:Created in 下午3:24 2019/4/2
+ * @ModifiedBy:
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    //https://jira.spring.io/browse/SPR-14651
+    //4.3.5 supports RedirectAttributes redirectAttributes
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+        return "redirect:/uploadStatus";
+    }
+
+}
